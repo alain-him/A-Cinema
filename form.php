0882df297f01b822c9b2df2d6b4eb86be748ba06
alain-him <byukusengealaindaddy@gmail.com>
2025-03-13 05:16:15 -1200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A!Cinema</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+    <nav>
+        <a href="form.html"><img src="logo.png" alt="logo"></a>
+    </nav>
+    <div class="form-wrapper">
+        <h2>Sign In</h2>
+        <form action="#">
+            <div class="form-control">
+                <input type="email" required>
+                <label>Email Or phone number</label>
+            </div>
+            <div class="form-control">
+                <input type="password" required>
+                <label>Password</label>
+            </div>
+            <button type="submit">Sign In</button>
+            <div class="form-help">
+                <div class="remember-me">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <a href="#">Need Help?</a>
+            </div>
+        </form>
+        <p>New to A!Cinema? <a href="signup.php">Sign up now</a></p>
+        <small>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, debitis mollitia. Blanditiis earum dicta aut odio commodi ullam. Ab ipsa illum accusantium, impedit harum numquam ducimus eaque nulla? Veritatis, adipisci.
+            <a href="#">Learn more</a>
+        </small>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A!Cinema</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+    <nav>
+        <a href="signup.html"><img src="logo.png" alt="logo"></a>
+    </nav>
+    <div class="form-wrapper">
+        <h2>Sign Up</h2>
+        <form action="#">
+            <div class="form-control">
+                <input type="text" required>
+                <label>Name</label>
+            </div>
+            <div class="form-control">
+                <input type="text" required>
+                <label>Username</label>
+            </div>
+            <div class="form-control">
+                <input type="email tel" required>
+                <label>Email Or Phone</label>
+            </div>
+            <div class="form-control">
+                <input type="password" required>
+                <label>Password</label>
+            </div>
+            <div class="form-control">
+                <input type="password" required>
+                <label>re-Password</label>
+            </div>
+            <button type="submit">Sign Up</button>
+            <div class="form-help">
+                <div class="remember-me">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <a href="#">Need Help?</a>
+            </div>
+        </form>
+        <p>New to A!Cinema? <a href="form.php">Sign In now</a></p>
+        <small>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, debitis mollitia. Blanditiis earum dicta aut odio commodi ullam. Ab ipsa illum accusantium, impedit harum numquam ducimus eaque nulla? Veritatis, adipisci.
+            <a href="#">Learn more</a>
+        </small>
+    </div>
+</body>
+</html>
